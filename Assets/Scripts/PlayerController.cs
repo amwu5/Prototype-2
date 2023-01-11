ec95c12b2b3bd8d113ecfbb56522fe66d4f6a562
amwu5 <121971706+amwu5@users.noreply.github.com>
2023-01-11 10:49:38 -0800

    Destroyed animals and food offscreen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,14 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -28,5 +30,10 @@ public class PlayerController : MonoBehaviour
         }
         horziontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horziontalInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
