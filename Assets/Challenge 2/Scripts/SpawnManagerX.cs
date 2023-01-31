90f9156d82482fa97d56481ddb5ff2ec399cfc79
amwu5 <121971706+amwu5@users.noreply.github.com>
2023-01-31 14:30:20 -0800

    continued on challenge
    random balls spawn now

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
-
     private float spawnLimitXLeft = -22.0f;
     private float spawnLimitXRight = 7.0f;
     private float spawnPosY = 30.0f;
@@ -23,10 +22,11 @@ public class SpawnManagerX : MonoBehaviour
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
